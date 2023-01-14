63ac93ec79fd691c110ad40066ff6dc9dc9427d5
mich1991 <wozniak.michal91@gmail.com>
2023-01-14 18:10:17 +0000

    Add Category,Post,Comment to Admin panel, add summernote to project

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Post, Category, Comment
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+	prepopulated_fields = {'slug': ('title',)}
+	summernote_fields = ('content')
+	list_filter = ('category', 'author', 'created_on',)
+	list_display = ('title', 'category', 'author', 'created_on',)
+
+
+admin.site.register(Category)
+admin.site.register(Comment)
