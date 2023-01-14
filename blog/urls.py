56c577168e855edb9855c3c4680211a7c4b8feb6
mich1991 <wozniak.michal91@gmail.com>
2023-01-14 19:22:26 +0000

    Add base.html, home page view, core url routes

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, PostDetailView, PostListView, PostLikeView, AboutPageView, ContactPageView
 urlpatterns = [
-	path('', HomePageView.as_view(), name='home')
+	path('', HomePageView.as_view(), name='home'),
+	path('posts', PostListView.as_view(), name='posts_list'),
+	path('posts/<slug:slug>', PostDetailView.as_view(), name='post_detail'),
+	path('like/<slug:slug>', PostLikeView.as_view(), name='post_like'),
+	path('about-us', AboutPageView.as_view(), name='about_us'),
+	path('contact-us', ContactPageView.as_view(), name='contact_us')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,52 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import Post
 
 
 class HomePageView(View):
+	def get(self, request):
+		published_posts = Post.objects.filter(status='1')
+		ps5_latest_posts = published_posts.filter(category__name='PS5').order_by('created_on')[:4]
+		xbox_latest_posts = published_posts.filter(category__name='XBOX').order_by('created_on')[:4]
+		switch_latest_posts = published_posts.filter(category__name='SWITCH').order_by('created_on')[:4]
+
+		ctx = {
+			'ps5': {
+				'latest_post': ps5_latest_posts.first(),
+				'recent_posts': ps5_latest_posts[1:]
+			},
+			'xbox': {
+				'latest_post': xbox_latest_posts.first(),
+				'recent_posts': xbox_latest_posts[1:]
+			},
+			'switch': {
+				'latest_post': switch_latest_posts.first(),
+				'recent_posts': switch_latest_posts[1:]
+			},
+		}
+		return render(request, 'blog/index.html', ctx)
+
+
+class PostListView(View):
+	def get(self):
+		pass
+
+
+class PostDetailView(View):
+	def get(self):
+		pass
+
+
+class PostLikeView(View):
+	def get(self):
+		pass
+
+
+class AboutPageView(View):
+	def get(self):
+		pass
+
+
+class ContactPageView(View):
 	def get(self):
 		pass
